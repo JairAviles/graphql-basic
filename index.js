0e789868b6991c7f0fbf6b18a80fec24b7017b8b
Jair Avilés <hi@jairaviles.mx>
2022-10-31 19:57:03 -0700

    add cors

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 const { makeExecutableSchema } = require('@graphql-tools/schema')
 const { graphqlHTTP } = require('express-graphql')
 const express = require('express')
+const cors = require('cors')
 const { readFileSync } = require('fs')
 const { join } = require('path')
 const resolvers = require('./lib/resolvers')
@@ -11,6 +12,7 @@ const resolvers = require('./lib/resolvers')
 // Express vars
 const app = express()
 const port = process.env.port || 3000
+const isDev = process.env.NODE_ENV !== 'production'
 
 // Defining initial schema
 const typeDefs = readFileSync(
@@ -23,9 +25,11 @@ const schema = makeExecutableSchema({
   resolvers
 })
 
+app.use(cors())
+
 app.use('/api', graphqlHTTP({
   schema,
-  graphiql: true
+  graphiql: isDev
 }))
 
 app.listen(port, () => {
